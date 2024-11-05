dfe760401fd03d246a12e240e837869d59ccd875
DevEarley <dev@asfunas.fun>
2024-11-05 13:39:14 -0500

    adding more profiling data

--- AdroitProfiler_Heartbeat.cs
+++ AdroitProfiler_Heartbeat.cs
@@ -12,7 +12,6 @@ public enum AdroitProfiler_Timing
     EveryQuarterSecond,
     EveryTenthSecond,
     InvokeAtTime,
-    InvokeOnSignal,
     InvokeDuringTimespan
 
 }
@@ -54,10 +53,6 @@ public class AdroitProfiler_Heartbeat: MonoBehaviour
     public delegate void On1s_Heartbeat();
     public List<On1s_Heartbeat> on1s_Heartbeat_delegates = new List<On1s_Heartbeat>();
 
-    public delegate void OnSignal(string signalMessage);
-    public List<OnSignal> onSignal_delegates = new List<OnSignal>();
-
-
     private void Start()
     {
         Paused = StartPaused;
@@ -93,13 +88,9 @@ public class AdroitProfiler_Heartbeat: MonoBehaviour
         Time.timeScale = 1.0f;
     }
 
-    public void Signal(string signalMessage)
-    {
-        onSignal_delegates.ForEach(x => x(signalMessage));
-    }
+
 
     private bool OneSecondSwitch = false;
-    private bool TwoSecondSwitch = false;
 
     private void CheckTimers()
     {
